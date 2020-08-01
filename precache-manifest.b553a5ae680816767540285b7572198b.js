dff17c67ad38a9e7bd0a9f05f3ba181b99fbbb4b
Michal Zborowski <historykus@interia.pl>
2020-08-01 18:55:57 +0200

    Updates

---
+++ precache-manifest.b553a5ae680816767540285b7572198b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35d41e2c4c110da613f3fe63be12bfb",
+    "url": "/Currency-Converter/index.html"
+  },
+  {
+    "revision": "4349f91679633bf49c43",
+    "url": "/Currency-Converter/static/css/main.05dce48a.chunk.css"
+  },
+  {
+    "revision": "664444cadbfc137bfc14",
+    "url": "/Currency-Converter/static/js/2.ae77f7fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Currency-Converter/static/js/2.ae77f7fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4349f91679633bf49c43",
+    "url": "/Currency-Converter/static/js/main.2b386de4.chunk.js"
+  },
+  {
+    "revision": "189d6bc29a85a59f9a1e",
+    "url": "/Currency-Converter/static/js/runtime-main.1deb8cb2.js"
+  },
+  {
+    "revision": "163943bc1e51d4d45974f4a451de4a7d",
+    "url": "/Currency-Converter/static/media/hand.163943bc.jpg"
+  }
+]);
